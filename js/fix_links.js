296cd6f1b00a383739906f1238938900ffec1652
daviseford <davis.e.ford.alt@gmail.com>
2015-02-02 17:48:21 -0500

    more cleaning

--- js/fix_links.js
+++ js/fix_links.js
@@ -33,22 +33,19 @@ $(document).ready(function() {
   
   	$("#keycodeForm").submit(function() {
 		event.preventDefault(); //Stops the page from reloading
-		keycodeGeneration = [
-		$("#inlinedate").val(), 
-		$("#list").val(), 
-		$("#email").val(), 
-		$("#product").val()	]; //This array stores our Keycode values, to be used shortly.
-		
-		console.log(keycodeGeneration.join("")); //Debugging
-		$("#submitKeycode").button({label: "Keycode Generated!"}); //Change button to reflect generation
+		keycodeGeneration = [$("#inlinedate").val(),$("#list").val(),$("#email").val(),$("#product").val()]; 	
+		//This array stores our Keycode values, to be used shortly.
+		$("#submitKeycode").button({label: "Keycode Generated!"}); //Change button label to reflect generation
 		$("#keycodeOutput").removeClass("alert-danger").addClass("alert-success").text("Generated Keycode: "+keycodeGeneration.join(""));
 		$("#keycodefield").val(keycodeGeneration.join(""));
 		
-		var x = $.trim(document.getElementById(keycodeGeneration[3]).value); 
+		var currProduct = $.trim(document.getElementById(keycodeGeneration[3]).value); 
 		//This grabs the element with the Product Code. 
+		//keycodeGeneration[3] = Product Selection
+		//Since the form ID's are set up with links as values, we can just grab the value of a matching form
+		//It's trimmed in the end just to be safe, not really necessary though
 		//For example - if keycodeGeneration[3] = SUB, this grabs the value of "#SUB", which is the link to the product
-		console.log(x); //Debugging
-		$("#ProductInput").val(x); //Sets up our product link in the "#inputForm"
+		$("#ProductInput").val(currProduct); //Sets up our product link in the "#inputForm"
 	});
   
   
@@ -68,7 +65,7 @@ $(document).ready(function() {
 			$("#productLinks").show();
 			
 			
-			var keycode = $.trim($("#keycodefield").val());
+			var keycode = $.trim($("#keycodefield").val()); //Establishing our keycode.
 			
 			//This pulls the whole UTM code together, being careful to trim the keycode of stray spaces
 			var utmsource = "?utm_source="+keycode+"&keycode="+keycode+"&u=[EMV FIELD]EMAIL_UUID[EMV /FIELD]";
@@ -87,14 +84,14 @@ $(document).ready(function() {
 					if(S(stmlCheck).contains('.stml')) {
 						var splitSTML = $.trim($(this).val().split('.stml')[0]);
 						$(this).val(splitSTML+".stml");	
-						console.log("Fixed string with stmlCheck")
+						console.log("Fixed string with stmlCheck"); //Not necessary, just for keeping track
 						}
 						
 				var htmlCheck = $.trim($(this).val())
 					if(S(htmlCheck).contains('.html')) {
 						var splitHTML = $.trim($(this).val().split('.html')[0]);
 						$(this).val(splitHTML+".html");	
-						console.log("Fixed string with htmlCheck")
+						console.log("Fixed string with htmlCheck"); //Not necessary, just for keeping track
 						}		
 			});
 			
