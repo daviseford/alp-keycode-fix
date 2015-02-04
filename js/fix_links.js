34391c5b8809fc726151d21d907ea267dd3fe420
daviseford <davis.e.ford.alt@gmail.com>
2015-02-04 15:57:18 -0500

    HUGE change to failure gate logic, KELLY PROOF

--- js/fix_links.js
+++ js/fix_links.js
@@ -1,31 +1,29 @@
 // JavaScript Document
-// var USRMM = "http://www.independentlivingnews.com/video/usr-video-3p.php"
-// var GAB = "http://www.independentlivingnews.com/il/great-american-blackout.php"
-// var DNS = "http://www.independentlivingnews.com/instant-access/digital-self-reliance-newsletter.stml"
-// var PW = "http://www.independentlivingnews.com/video/pw-video.php"
-// var thirtyDay ="http://www.independentlivingnews.com/video/comfort-food-reserve.php"
-// var CSG = "https://www.independentlivingnews.com/video/csg-video.php"
-// var gunControl = "http://www.independentlivingnews.com/poll/gun-control/poll.php"
-// var LPLM = "http://www.independentlivingnews.com/video/lpl-video.php"
-// var ObamaDollar = "http://www.independentlivingnews.com/il/obama-dollar-debacle.php"
-// var emergencyPack = "http://www.independentlivingnews.com/video/epack2-video.php"
-// var sunTrek = "http://www.independentlivingnews.com/video/suntrek/" 
-
-
 
 $(document).ready(function() {
 	$("#linkResults").hide(); //Sloppily hiding these two divs.
 	$("#productLinks").hide();
 	
-	//Find value of X inputs and store them in an "Original" array. 
-	//Do this before appending utms - you could just spit out "console.log(original[x]). 
-	//Then call this if a keycode is regenerating, restoring product links to the original ones
 	
-		var values = {};
-		$('#productForm input').each(function() {
-    	values[this.name] = this.value;
-		});
-		console.log(values);
+	function stmlCheckFunc(){
+		var inputVal = $.trim($(this).val())
+			if(S(inputVal).contains('.stml')) {
+				var splitSTML = $.trim($(this).val().split('.stml')[0]); //split the value into two parts of an array. 
+				$(this).val(splitSTML+".stml");	//re-add the .stml ending
+				console.log("Fixed string with stmlCheck"); //Not necessary, just for keeping track
+			} else if(S(inputVal).contains('.html')) {
+				var splitHTML = $.trim($(this).val().split('.html')[0]);
+				$(this).val(splitHTML+".html");	
+				console.log("Fixed string with htmlCheck"); //Not necessary, just for keeping track
+			} else if(S(inputVal).contains('?utm_source')) {
+				var splitUTM = $.trim($(this).val().split('?utm_source')[0]);
+				$(this).val(splitUTM);	
+				console.log("Fixed string with utmCheck"); //Not necessary, just for keeping track
+				}
+			
+	}; 
+	//The stmlCheckFunc scrubs links of anything extending past .html and .stml endings.
+	//Additionally, it strips existing UTM codes away, which is Kelly-proof (hopefully)
 		
 	
 	//Setting up the keycode-generating menus
@@ -40,13 +38,13 @@ $(document).ready(function() {
 		dateFormat: "ymmdd" //Outputs as YYMMDD
 		});
 	
-  
-  	$("#keycodeForm").submit(function() {
+  	//This handles generating the keycode. It simply joins all of the necessary values from an array.
+  	$("#keycodeForm").submit(function() { 
 		event.preventDefault(); //Stops the page from reloading
 		keycodeGeneration = [$("#inlinedate").val(),$("#list").val(),$("#email").val(),$("#product").val()]; 	
 		//This array stores our Keycode values, to be used shortly.
 		$("#submitKeycode").button({label: "Keycode Generated!"}); //Change button label to reflect generation
-		$("#keycodeOutput").removeClass("alert-danger").addClass("alert-success").text("Generated Keycode: "+keycodeGeneration.join(""));
+		$("#keycodeOutput").removeClass("alert-danger").effect("highlight", "slow").addClass("alert-success").text("Generated Keycode: "+keycodeGeneration.join(""));
 		$("#keycodefield").val(keycodeGeneration.join(""));
 		
 		var currProduct = $.trim(document.getElementById(keycodeGeneration[3]).value); 
@@ -59,8 +57,7 @@ $(document).ready(function() {
 	});
   
   
-				
-	//start checking for valid input
+	//This function controls data validation, link scrubbing, and data transfer for the "Generate Links" button			
 	$("#inputForm").submit(function() {
 		event.preventDefault();
 		if($("#keycodefield").val() == ""){
@@ -71,40 +68,24 @@ $(document).ready(function() {
 				alert("You must enter at least one link! That's pretty obvious, at least to my primitive computer brain.");
 			}
 			else {
-			$("#linkResults").show();
-			$("#productLinks").show();
+			//Trim old STML, UTM, and HTML codes.	
+			$("#inputForm input").each(stmlCheckFunc); //This trims #inputForm - which in turn takes care of #linkResults
+			$("#productForm input").each(stmlCheckFunc); //This takes care of the product links, which previously were being stacked with multiple utm codes.
+			
+			$("#linkResults").show("drop"); //Show our results.
+			$("#productLinks").show("drop");
 			
 			
 			var keycode = $.trim($("#keycodefield").val()); //Establishing our keycode.
-			
+			var utmsource = "?utm_source="+keycode+"&keycode="+keycode+"&u=[EMV FIELD]EMAIL_UUID[EMV /FIELD]"; 	
 			//This pulls the whole UTM code together, being careful to trim the keycode of stray spaces
-			var utmsource = "?utm_source="+keycode+"&keycode="+keycode+"&u=[EMV FIELD]EMAIL_UUID[EMV /FIELD]";
+			
 			// This function iterates over each Product, appending the URL with our UTM
 			$("#productForm input").each(function() {
-  				var currVal = $.trim($(this).val())
+  				var currVal = $.trim($(this).val());	
 				$(this).val(currVal+utmsource);
 			});
-			
-			
-			//Here we split the link1 value into two parts of an array. 
-			//We do this to remove any unnecessary trailing codes from ILN links. 
-			//Kelly also has a tendency to copy+paste utm codes into this, so hopefully that will help.
-			$("#inputForm input").each(function() {
-  				var stmlCheck = $.trim($(this).val())
-					if(S(stmlCheck).contains('.stml')) {
-						var splitSTML = $.trim($(this).val().split('.stml')[0]);
-						$(this).val(splitSTML+".stml");	
-						console.log("Fixed string with stmlCheck"); //Not necessary, just for keeping track
-						}
-						
-				var htmlCheck = $.trim($(this).val())
-					if(S(htmlCheck).contains('.html')) {
-						var splitHTML = $.trim($(this).val().split('.html')[0]);
-						$(this).val(splitHTML+".html");	
-						console.log("Fixed string with htmlCheck"); //Not necessary, just for keeping track
-						}		
-			});
-			
+									
 			//Below, we print the first link, and then check the other inputs for value.
 			//If the input has something, it is processed. Otherwise, it's left alone.
 			if($("#link1").val() != "") {
